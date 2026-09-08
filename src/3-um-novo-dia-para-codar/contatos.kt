0d058946c26da7fade0bcc7366f8c2e6221053aa
zerowavez <lucas.tpsilva@senacsp.edu.br>
2026-09-08 12:58:52 -0300

    fundações para teralbitha

--- src/3-um-novo-dia-para-codar/contatos.kt
+++
@@ -1,8 +0,0 @@
-//Crie um programa que funcione como uma agenda de contatos.
-
-//O usuário pode cadastrar contatos informando nome e telefone (armazene como objetos em um array).
-//O programa deve permitir que o usuário busque um contato pelo nome.
-//Se o contato existir, exiba o telefone; se não existir, exiba "Contato não encontrado".
-//O usuário também pode remover um contato pelo nome.
-//Quando o usuário digitar "LISTAR", exiba todos os contatos cadastrados (nome e telefone).
-//Quando o usuário digitar "SAIR", encerre o programa exibindo a mensagem "Agenda encerrada. Total de contatos: X".
